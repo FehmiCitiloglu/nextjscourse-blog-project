98064e9819a148c8f09f7360042d41c25a6a7687
FehmiCitiloglu <fehmicitiloglu@gmail.com>
2022-02-05 23:59:58 +0300

    Loader Configuration

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   reactStrictMode: true,
-    basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-    assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH
-}
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
+  images: {
+    loader: "imgix",
+    path: "https://fehmicitiloglu.github.io/nextjscourse-blog-project/",
+  },
+};
